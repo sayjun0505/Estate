5a26cbc20722507f12cca35ae323c399357ad02e
sayjun <sayjun0505@gmail.com>
2023-07-19 22:23:13 +0800

    next button on progress

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,26 +1,40 @@
 import React, { useState, useEffect } from "react"
 import ImageCrossFade from "./ImageCrossFade"
-import wait from "waait"
-
+// import { useSpring, animated } from 'react-spring';
 export default function BackgroundImage() {
-
-  const [image, setImage] = useState<number>(0)
-
+  const [image, setImage] = useState<number>(0);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  }, []);
   const images = [
     '/sina-bahar-_BsZKOlhuyo-unsplash.jpg',
     '/maciek-sulkowski-gmejHJ6k-VY-unsplash.jpg',
     '/itadaki-jEeksuy9SMk-unsplash.jpg',
-  ]
+  ];
 
   useEffect(() => {
-    wait(4000).then(() => {
-      setImage((image + 1) % 3)
-    })
-  }, [image])
-
+    const timer = setTimeout(() => {
+      setImage((image + 1) % images.length);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [image, images]);
+  // const animation = useSpring({
+  //   opacity: show ? 1 : 0,
+  //   transform: show ? 'translateX(0)' : 'translateX(100vw)',
+  // });
   return (
-    <ImageCrossFade
-      imgUrl={images[image]} width={1728} height={864}
-    />
-  )
+    <> 
+      <ImageCrossFade
+        imgUrl={images[image]}
+        width={1728}
+        height={864}
+      />
+      {/* <animated.div style={animation}>
+        <h1 className="text-2xl text-white">VILLA</h1>
+        <h1 className="text-2xl text-white">ESPEJO AOUA</h1>
+      </animated.div> */}
+    </>
+
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from 'next/image';
 import BackgroundImage from "../components/BackgroundImage";
 
 const Home: NextPage = () => {
+  
+  const nextstep=()=>{
+    alert("Why click me? Don't disturb me.")
+  }
+  
+
   return (
     <>
       <Head>
         <title>Image Crossfade</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-[100vh]">
+      <div className="h-[100vh] ">
         <main>
-          <div className="container mx-auto px-36 pt-12">
+          <div className="container mx-auto px-36 pt-12 ">
             <div className="flex text-white justify-between">
               <div className="">Real Estate</div>
               <div className=" w-2/5 header-search-input rounded-full">
@@ -20,7 +27,15 @@ const Home: NextPage = () => {
               <div className="">sdfs</div>
             </div>
           </div>
-
+          <div className='text-white  mt-[60vh] mr-[10vw]  flex justify-end items-end'>
+            <div onClick={nextstep} className='flex flex-col hover:cursor-pointer border-2 border-red-500 justify-center items-center'> 
+              <Image src="/next-right-arrow.svg" width={100} height={10} alt="Next Arrow" className='rightArrow' />              
+              <p className='text-2xl text-white '>Next</p>
+            </div>
+          </div>
+          <div>
+      
+    </div>
           <BackgroundImage />
         </main>
       </div>
