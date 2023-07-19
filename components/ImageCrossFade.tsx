fda3eeedd7589b8dd9821eff11c67fb506c77722
athenaweb629 <athenaweb629@gmail.com>
2023-07-19 23:24:49 +0900

    animation on progress

--- components/ImageCrossFade.tsx
+++ components/ImageCrossFade.tsx
@@ -21,21 +21,21 @@ const ImageCrossFade = ({ imgUrl, width, height, }: ImageCrossFadeProps) => {
   return (
     <div className="absolute top-0 z-[-1] left-0 w-full bg-black">
       {fadeIn && (
-        <Image
-          onLoadingComplete={() => {
-            setLoaded(true)
-            wait(1100).then(() => {
-              setLoadedImage(imgUrl)
-            })
-          }}
-          alt=""
-          src={imgUrl}
-          priority
-          width={1728}
-          height={864}
-          className={`w-full absolute top-0 left-0 z-10 transition-opacity duration-1000 ${loaded ? "opacity-100" : "opacity-0"
-            }`}
-        />
+          <Image
+            onLoadingComplete={() => {
+              setLoaded(true)
+              wait(1100).then(() => {
+                setLoadedImage(imgUrl)
+              })
+            }}
+            alt=""
+            src={imgUrl}
+            priority
+            width={1728}
+            height={864}
+            className={`w-full absolute top-0 left-0 z-10 transition-opacity duration-1000 ${loaded ? "opacity-100" : "opacity-0"
+              }`}
+          />
       )}
       {
         loadedImage && (<Image
@@ -50,13 +50,7 @@ const ImageCrossFade = ({ imgUrl, width, height, }: ImageCrossFadeProps) => {
           className={`w-full`}
         />)
       }
-      <div
-        className="w-full h-full absolute left-0 bottom-0"
-        style={{
-          background:
-            "linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 50%)",
-        }}
-      ></div>
+      
     </div>
   )
 }
