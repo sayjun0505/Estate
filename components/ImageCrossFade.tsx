2ff0387618bd1755fb43513b7fb53669d5576c5b
athenaweb629 <athenaweb629@gmail.com>
2023-07-20 20:08:53 +0900

    almost done

--- components/ImageCrossFade.tsx
+++ components/ImageCrossFade.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
-import Image from "next/image"
-import wait from "waait"
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import wait from "waait";
 
 interface ImageCrossFadeProps {
   imgUrl: string;
@@ -8,51 +8,65 @@ interface ImageCrossFadeProps {
   height: number;
 }
 
-const ImageCrossFade = ({ imgUrl, width, height, }: ImageCrossFadeProps) => {
-  const [fadeIn, setFadeIn] = useState<boolean>(false)
-  const [loaded, setLoaded] = useState<boolean>(false)
-  const [loadedImage, setLoadedImage] = useState<string>(imgUrl)
+const ImageCrossFade = ({ imgUrl, width, height }: ImageCrossFadeProps) => {
+  const [fadeIn, setFadeIn] = useState<boolean>(false);
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [loadedImage, setLoadedImage] = useState<string>(imgUrl);
 
   useEffect(() => {
-    setLoaded(false)
-    setFadeIn(true)
-  }, [imgUrl])
+    setLoaded(false);
+    setFadeIn(true);
+  }, [imgUrl]);
 
   return (
     <div className="absolute top-0 z-[-1] left-0 w-full bg-black">
       {fadeIn && (
+        <div
+          className={`w-full absolute top-0 left-0 z-10 transition-opacity duration-1000 ${
+            loaded ? "opacity-100" : "opacity-0"
+          }`}
+          style={{ backgroundImage: `url(${imgUrl})`, height: "100vh", backgroundSize: "cover" }}
+        >
           <Image
             onLoadingComplete={() => {
-              setLoaded(true)
+              setLoaded(true);
               wait(1100).then(() => {
-                setLoadedImage(imgUrl)
-              })
+                setLoadedImage(imgUrl);
+              });
             }}
             alt=""
             src={imgUrl}
             priority
-            width={1728}
-            height={864}
-            className={`w-full absolute top-0 left-0 z-10 transition-opacity duration-1000 ${loaded ? "opacity-100" : "opacity-0"
-              }`}
+            width={0}
+            height={0}
+            style={{ display: "none" }}
           />
+        </div>
       )}
-      {
-        loadedImage && (<Image
-          onLoadingComplete={() => {
-            setFadeIn(false)
+      {loadedImage && (
+        <div
+          style={{
+            backgroundImage: `url(${loadedImage})`,
+            height: "100vh",
+            backgroundSize: "cover",
           }}
-          alt=""
-          src={loadedImage}
-          priority
-          width={width}
-          height={height}
           className={`w-full`}
-        />)
-      }
-      
+        >
+          <Image
+            onLoadingComplete={() => {
+              setFadeIn(false);
+            }}
+            alt=""
+            src={loadedImage}
+            priority
+            width={0}
+            height={0}
+            style={{ display: "none" }}
+          />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default React.memo(ImageCrossFade)
+export default React.memo(ImageCrossFade);
